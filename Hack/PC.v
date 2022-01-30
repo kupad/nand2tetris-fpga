6d4b7d22f0a14854feba4620e00df25a29ff6a5f
kupad <dreizen@kupad.net>
2022-01-30 14:45:30 -0500

    my implementation of the Program Counter

--- Hack/PC.v
+++ Hack/PC.v
@@ -1,9 +1,12 @@
 /**
- * A 16-bit counter with load control bit.
- * if (load[t] == 1)       out[t+1] = in[t]
- * else if (inc[t] == 1)   out[t+1] = out[t] + 1  (integer addition)
- * else                    out[t+1] = out[t]
- */
+if (reset[t] == 1) out[t+1] = 0
+	else if (load[t] == 1) out[t+1] = in
+	else if (inc[t] == 1) out[t+1] = out[t] + 1 (integer add)
+	else out[t+1] = out[t]
+*/
+//NOTE: The provided PC.out and PC.cmp do not match. I saved
+//the PC.out as PC.out.orig. Comparing that to my own PC.out
+//shows it's the same. So does comparing the testbunch results
 
 `default_nettype none
 
@@ -16,6 +19,18 @@ module PC(
 	output wire [15:0] out
 );	
 
-
+    reg [15:0] curr = 0, next = 0;
+    always @(negedge clk) begin
+        curr <= next;
+        if (reset)
+            next <= 0;
+        else if(load) 
+            next <= in;
+        else if (inc)
+            next <= next + 1;
+        else
+            next <= next;
+    end
+    assign out = curr;
 
 endmodule
